e87df6c5b2c7e2922ce8733ce43fdfd02595752c
Adam Lomas <adamp@marcatodigital.com>
2014-09-29 17:08:01 -0400

    implement following and unfollowing

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -32,6 +32,17 @@ describe "Static pages" do
         end
       end
 
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) } 
+        it { should have_link("1 followers", href: followers_user_path(user)) }
+      end
+      
       it { should have_content("microposts") } # only checking the pluralized here since I think we can assume if .pluralize is working then the singular of same should also work.
 
       describe "pagination" do
