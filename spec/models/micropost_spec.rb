655ae2e7358402640b5dff2f4c0a30ec01cec7b8
Adam Lomas <adamp@marcatodigital.com>
2014-10-01 19:54:12 -0400

    Write tests for @replies

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -27,4 +27,24 @@ describe Micropost do
     before { @micropost.content = "a" * 141 }
     it { should_not be_valid }
   end
+
+  describe "@replies" do
+    before do
+      @recipient = FactoryGirl.create(:user, username: "bieber")
+      @other_user = FactoryGirl.create(:user)
+      @reply = FactoryGirl.create(:micropost, user: user, content: "@bieber is the best")
+      @recipient.follow!(user)
+      @other_user.follow!(user)
+    end
+
+    describe "should appear in recipient's feed" do
+      subject { @recipient.feed }
+      it { should include(@reply) }
+    end
+
+    describe "should not appear in other users feeds" do
+      subject { @other_user.feed }
+      it { should_not include(@reply) }
+    end
+  end
 end
